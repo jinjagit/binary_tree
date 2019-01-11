52295f76a3da3aa8b240c19e11d52576b343396a
Simon Tharby <jinjamail@gmail.com>
2019-01-11 21:37:42 +0000

    create add_node(value) def in Tree class

--- tree.rb
+++ tree.rb
@@ -19,4 +19,42 @@ class Tree
     @root = nil
     @size = 0
   end
+
+  def add_node(value)
+    if @root == nil
+      @root = Node.new(value)
+      @size += 1
+    else
+      node = @root
+      added = false
+      while added == false do
+        if value > node.value
+          if node.r_child == nil
+            node.r_child = Node.new(value)
+            added = true
+          else
+            node = node.r_child
+          end
+        elsif value <= node.value
+          if node.l_child == nil
+            node.l_child = Node.new(value)
+            added = true
+          else
+            node = node.l_child
+          end
+        end
+      end
+      @size += 1
+    end
+  end
 end
+
+ary = []
+20.times {ary << rand(99)}
+p ary
+
+tree = Tree.new
+
+ary.each {|e| tree.add_node(e)}
+
+p tree.size

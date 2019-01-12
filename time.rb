62dc570e1916e246633294fbc0789c3c41be6cf6
Simon Tharby <jinjamail@gmail.com>
2019-01-12 17:28:21 +0000

    add measurement of dfs time complexity

--- time.rb
+++ time.rb
@@ -16,6 +16,7 @@ end
 
 ary_search = []
 bfs_search = []
+dfs_search = []
 
 1000.times do
   ary = []
@@ -26,17 +27,22 @@ bfs_search = []
   tree = Tree.new(ary)
   node, count = tree.breadth_first_search(target)
   bfs_search << count / 200.0
+  node, count = tree.depth_first_search(target)
+  dfs_search << count / 200.0
 end
 
 ary_average = ary_search.inject(0){|sum,x| sum + x } / 1000.0
 bfs_average = bfs_search.inject(0){|sum,x| sum + x } / 1000.0
+dfs_average = dfs_search.inject(0){|sum,x| sum + x } / 1000.0
 
 puts "array search v / n = #{ary_average}"
-puts "  bfs search v / n = #{bfs_average}"
+puts "         bfs v / n = #{bfs_average}"
+puts "         dfs v / n = #{dfs_average}"
 
 # --------------------- example output --------------------------------
 
 # => array search v / n = 0.47309499999999965
-# =>   bfs search v / n = 0.50067
+# =>          bfs v / n = 0.50067
+# =>          dfs v / n = 0.4915750000000003
 
-# (over multiple runs neither method varies much from v / n = 0.5)
+# (over multiple runs no method varies much from v / n = 0.5)

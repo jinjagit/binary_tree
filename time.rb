b165db5d6d1a13e1b49fee23ed609bded3d67cb0
Simon Tharby <jinjamail@gmail.com>
2019-01-12 22:56:39 +0000

    add measurement of conditional search

--- time.rb
+++ time.rb
@@ -18,6 +18,7 @@ ary_search = []
 bfs_search = []
 dfs_search = []
 dfs_rec_search = []
+conditional_search = []
 
 1000.times do
   ary = []
@@ -32,23 +33,29 @@ dfs_rec_search = []
   dfs_search << count / 200.0
   node, count = tree.dfs_rec_count(target)
   dfs_rec_search << count / 200.0
+  node, count = tree.conditional_search(target)
+  conditional_search << count / 200.0
 end
 
 ary_average = ary_search.inject(0){|sum,x| sum + x } / 1000.0
 bfs_average = bfs_search.inject(0){|sum,x| sum + x } / 1000.0
 dfs_average = dfs_search.inject(0){|sum,x| sum + x } / 1000.0
 dfs_rec_average = dfs_rec_search.inject(0){|sum,x| sum + x } / 1000.0
+conditional_average = conditional_search.inject(0){|sum,x| sum + x } / 1000.0
 
-puts "array search v / n = #{ary_average}"
-puts "         bfs v / n = #{bfs_average}"
-puts "         dfs v / n = #{dfs_average}"
-puts "     dfs_rec v / n = #{dfs_rec_average}"
+puts "        array search v / n = #{ary_average}"
+puts "breadth first search v / n = #{bfs_average}"
+puts "  depth first search v / n = #{dfs_average}"
+puts "       recursive dfs v / n = #{dfs_rec_average}"
+puts "  conditional search v / n = #{conditional_average}"
 
 # --------------------- example output --------------------------------
 
-# => array search v / n = 0.4715249999999997
-# =>          bfs v / n = 0.4937999999999996
-# =>          dfs v / n = 0.4927200000000004
-# =>      dfs_rec v / n = 0.4678399999999995
+# =>         array search v / n = 0.46781499999999965
+# => breadth first search v / n = 0.48208999999999963
+# =>   depth first search v / n = 0.5117750000000004
+# =>        recursive dfs v / n = 0.48771999999999976
+# =>   conditional search v / n = 0.04306500000000002
 
-# (over multiple runs no method varies much from v / n = 0.5)
+# over multiple runs only the conditional method varies much from v / n = 0.5
+# and by a lot! (over 10 times 'faster' in terms of operations)

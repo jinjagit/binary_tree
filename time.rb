556d965c13a375f6b58e1d40efcdbb134e82fe7b
Simon Tharby <jinjamail@gmail.com>
2019-01-12 20:05:10 +0000

    add measurement of recursive dfs time complexity

--- time.rb
+++ time.rb
@@ -17,6 +17,7 @@ end
 ary_search = []
 bfs_search = []
 dfs_search = []
+dfs_rec_search = []
 
 1000.times do
   ary = []
@@ -29,20 +30,25 @@ dfs_search = []
   bfs_search << count / 200.0
   node, count = tree.depth_first_search(target)
   dfs_search << count / 200.0
+  node, count = tree.dfs_rec_count(target)
+  dfs_rec_search << count / 200.0
 end
 
 ary_average = ary_search.inject(0){|sum,x| sum + x } / 1000.0
 bfs_average = bfs_search.inject(0){|sum,x| sum + x } / 1000.0
 dfs_average = dfs_search.inject(0){|sum,x| sum + x } / 1000.0
+dfs_rec_average = dfs_rec_search.inject(0){|sum,x| sum + x } / 1000.0
 
 puts "array search v / n = #{ary_average}"
 puts "         bfs v / n = #{bfs_average}"
 puts "         dfs v / n = #{dfs_average}"
+puts "     dfs_rec v / n = #{dfs_rec_average}"
 
 # --------------------- example output --------------------------------
 
-# => array search v / n = 0.47309499999999965
-# =>          bfs v / n = 0.50067
-# =>          dfs v / n = 0.4915750000000003
+# => array search v / n = 0.4715249999999997
+# =>          bfs v / n = 0.4937999999999996
+# =>          dfs v / n = 0.4927200000000004
+# =>      dfs_rec v / n = 0.4678399999999995
 
 # (over multiple runs no method varies much from v / n = 0.5)
